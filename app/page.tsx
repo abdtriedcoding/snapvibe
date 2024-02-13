0004055a3dc2e155260ec1459d9736a4679452c2
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-14 00:11:35 +0530

    authentication section complete✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
+import { auth } from "@/lib/auth";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  const session = await auth();
   return <>{JSON.stringify(session)}</>;
 }
