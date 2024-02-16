c5d127ff1d25f0ff6f654cec4c861dcb56d30b9d
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-16 18:29:25 +0530

    working on create post page

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import useMount from "@/hook/useMount";
+import { usePathname, useRouter } from "next/navigation";
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+  const mount = useMount();
+  const isCreatePage = pathname === "/dashboard/create";
+  if (!mount) return null;
+  return (
+    <Dialog open={isCreatePage} onOpenChange={(open) => !open && router.back()}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreatePage;
