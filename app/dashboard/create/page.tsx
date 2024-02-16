be79dcebff664da4bf7b204bf58d75495f95096d
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-16 19:00:56 +0530

    add dialog for creating post

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,30 +1,111 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { z } from "zod";
+import Image from "next/image";
 import useMount from "@/hook/useMount";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 import { usePathname, useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { CreatePost } from "@/lib/schema";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const CreatePage = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof CreatePost>>({
+    resolver: zodResolver(CreatePost),
+    defaultValues: {
+      caption: "",
+      fileUrl: undefined,
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof CreatePost>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   const pathname = usePathname();
   const router = useRouter();
   const mount = useMount();
   const isCreatePage = pathname === "/dashboard/create";
   if (!mount) return null;
+
+  const fileUrl = form.watch("fileUrl");
+
   return (
     <Dialog open={isCreatePage} onOpenChange={(open) => !open && router.back()}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              {!!fileUrl ? (
+                <div className="h-96 md:h-[450px] overflow-hidden rounded-md">
+                  <AspectRatio ratio={1 / 1} className="relative h-full">
+                    <Image
+                      src={fileUrl}
+                      alt="Post preview"
+                      fill
+                      className="rounded-md object-cover"
+                    />
+                  </AspectRatio>
+                </div>
+              ) : (
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field, fieldState }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="picture">Picture</FormLabel>
+                      <FormControl>
+                        <p>Todo upload file</p>
+                      </FormControl>
+                      <FormDescription>
+                        Upload a picture to post.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+
+              <FormField
+                control={form.control}
+                name="caption"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="caption">Caption</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="caption"
+                        id="caption"
+                        placeholder="Write a caption..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" disabled={form.formState.isSubmitting}>
+                Create Post
+              </Button>
+            </form>
+          </Form>
         </DialogHeader>
       </DialogContent>
     </Dialog>
