f51a75bb633d84a8bb6c2440cc42fab5eb51d277
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-18 18:14:18 +0530

    add server action to fetch post by id

---
+++ app/actions/fetchPostById.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import prisma from "@/lib/prisma";
+import { unstable_noStore } from "next/cache";
+
+export async function fetchPostById(id: string) {
+  unstable_noStore();
+
+  try {
+    const data = await prisma.post.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        comments: {
+          include: {
+            user: true,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+        likes: true,
+        savedBy: true,
+        user: true,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch post");
+  }
+}
