5db8b23a5bdfdb8cc7882cd706dfca8e68ad447a
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-02-18 14:21:49 +0530

    create server action for adding comment

---
+++ app/actions/createComment.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import { getUserId } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
+
+export async function createComment(postId: string, body: string) {
+  const userId = await getUserId();
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  try {
+    await prisma.comment.create({
+      data: {
+        body,
+        postId,
+        userId,
+      },
+    });
+    revalidatePath("/dashboard");
+    return { message: "Created Comment." };
+  } catch (error) {
+    return { message: "Failed to Create Comment." };
+  }
+}
