cb785d2919c161f59b88197cb83688f4e98acc2b
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-07-16 02:00:23 +0530

    using callbacks to modify auth session

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,49 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token }) {
+      const prismaUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!prismaUser) {
+        return token
+      }
+
+      if (!prismaUser.username) {
+        await prisma.user.update({
+          where: {
+            id: prismaUser.id,
+          },
+          data: {
+            username: prismaUser.name?.split(' ').join('').toLowerCase(),
+          },
+        })
+      }
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        email: prismaUser.email,
+        username: prismaUser.username,
+        picture: prismaUser.image,
+      }
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user.username = token.username as string
+      }
+
+      return session
+    },
+  },
   pages: {
     signIn: '/login',
   },
