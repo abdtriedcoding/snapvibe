4ba922accea865a848bdb6cf968ba86a6d1dc51a
abdtriedcoding <siddabdullahofficial@gmail.com>
2024-07-16 18:49:32 +0530

    refactor edit profile page

--- app/dashboard/_components/profile-form.tsx
+++ app/dashboard/_components/profile-form.tsx
@@ -1,5 +1,17 @@
-"use client";
+'use client'
 
+import { type z } from 'zod'
+import { toast } from 'sonner'
+import { UserSchema } from '@/lib/schema'
+import { useForm } from 'react-hook-form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { type UserProfile } from '@/lib/definitions'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { updateProfile } from '@/app/actions/updateProfile'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import ProfileAvatarDialog from '../[username]/_components/profile-avatar-dialog'
 import {
   Form,
   FormControl,
@@ -8,52 +20,52 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form";
+} from '@/components/ui/form'
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+} from '@/components/ui/select'
 
-import { z } from "zod";
-import { toast } from "sonner";
-import { UserSchema } from "@/lib/schema";
-import { useForm } from "react-hook-form";
-import { UserProfile } from "@/lib/definitions";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { updateProfile } from "@/app/actions/updateProfile";
-import ProfileAvatarDialog from "../[username]/_components/profile-avatar-dialog";
-
-function ProfileForm({ profile }: { profile: UserProfile }) {
+export default function ProfileForm({
+  profile,
+  userId,
+}: {
+  profile: UserProfile
+  userId: string | undefined
+}) {
   const form = useForm<z.infer<typeof UserSchema>>({
     resolver: zodResolver(UserSchema),
     defaultValues: {
-      id: profile.id,
-      image: profile.image || "",
-      name: profile.name || "",
-      username: profile.username || "",
-      bio: profile.bio || "",
-      gender: profile.gender || "",
-      website: profile.website || "",
+      image: profile.image ?? '',
+      name: profile.name ?? '',
+      username: profile.username ?? '',
+      bio: profile.bio ?? '',
+      gender: profile.gender ?? '',
+      website: profile.website ?? '',
     },
-  });
+  })
 
-  const { isDirty, isSubmitting, isValid } = form.formState;
+  const { isDirty, isSubmitting, isValid } = form.formState
+
+  function onSubmit(data: z.infer<typeof UserSchema>) {
+    toast.promise(updateProfile(data), {
+      loading: 'Updating profile...',
+      success: 'Profile updated successfully',
+      error: 'Failed to update profile',
+    })
+  }
 
   return (
-    <div className="space-y-8 py-10 lg:p-10 max-w-xl">
+    <div className="max-w-xl space-y-8 py-10 lg:p-10">
       <div className="flex items-center gap-x-2 md:gap-x-5">
-        <ProfileAvatarDialog user={profile}>
-          <div className="md:w-20 flex md:justify-end">
-            <Avatar className="w-11 h-11 cursor-pointer">
+        <ProfileAvatarDialog profile={profile} userId={userId}>
+          <div className="flex md:w-20 md:justify-end">
+            <Avatar className="h-11 w-11 cursor-pointer">
               <AvatarImage
-                src={profile.image ?? "https://github.com/shadcn.png"}
+                src={profile.image ?? 'https://github.com/shadcn.png'}
               />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
@@ -61,8 +73,8 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
         </ProfileAvatarDialog>
         <div>
           <p className="font-medium">{profile.username}</p>
-          <ProfileAvatarDialog user={profile}>
-            <p className="text-blue-500 text-sm font-bold cursor-pointer hover:text-white">
+          <ProfileAvatarDialog profile={profile} userId={userId}>
+            <p className="cursor-pointer text-sm font-bold text-blue-500 hover:text-white">
               Change profile photo
             </p>
           </ProfileAvatarDialog>
@@ -70,20 +82,14 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
       </div>
 
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(async (values) => {
-            const { message } = await updateProfile(values);
-            toast(message);
-          })}
-          className="space-y-8"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
             name="website"
             render={({ field }) => (
               <FormItem>
-                <div className="flex flex-col md:flex-row md:items-center gap-y-2 gap-x-8">
-                  <FormLabel className="font-bold w-20 md:text-right">
+                <div className="flex flex-col gap-x-8 gap-y-2 md:flex-row md:items-center">
+                  <FormLabel className="w-20 font-bold md:text-right">
                     Website
                   </FormLabel>
                   <FormControl aria-disabled>
@@ -100,15 +106,15 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
             name="bio"
             render={({ field }) => (
               <FormItem>
-                <div className="flex flex-col md:flex-row md:items-center gap-y-2 gap-x-8">
-                  <FormLabel className="font-bold w-20 md:text-right">
+                <div className="flex flex-col gap-x-8 gap-y-2 md:flex-row md:items-center">
+                  <FormLabel className="w-20 font-bold md:text-right">
                     Bio
                   </FormLabel>
                   <FormControl>
                     <Textarea className="resize-none" {...field} />
                   </FormControl>
                 </div>
-                <FormDescription className="md:ml-24 text-xs">
+                <FormDescription className="text-xs md:ml-24">
                   {field.value?.length} / 150
                 </FormDescription>
                 <FormMessage className="md:ml-24" />
@@ -121,8 +127,8 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
             name="gender"
             render={({ field }) => (
               <FormItem>
-                <div className="flex flex-col md:flex-row md:items-center gap-y-2 gap-x-8">
-                  <FormLabel className="font-bold w-20 md:text-right">
+                <div className="flex flex-col gap-x-8 gap-y-2 md:flex-row md:items-center">
+                  <FormLabel className="w-20 font-bold md:text-right">
                     Gender
                   </FormLabel>
                   <Select
@@ -143,7 +149,7 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
                     </SelectContent>
                   </Select>
                 </div>
-                <FormDescription className="md:ml-24 text-xs">
+                <FormDescription className="text-xs md:ml-24">
                   This wont be part of your public profile.
                 </FormDescription>
                 <FormMessage className="md:ml-24" />
@@ -161,7 +167,5 @@ function ProfileForm({ profile }: { profile: UserProfile }) {
         </form>
       </Form>
     </div>
-  );
+  )
 }
-
-export default ProfileForm;
--- app/dashboard/edit-profile/page.tsx
+++ app/dashboard/edit-profile/page.tsx
@@ -1,29 +1,30 @@
-import { Metadata } from "next";
-import { auth } from "@/lib/auth";
-import { notFound } from "next/navigation";
-import { fetchProfile } from "@/app/actions/fetchUserProfile";
-import ProfileForm from "../_components/profile-form";
+import { auth } from '@/lib/auth'
+import { type Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import { fetchProfile } from '@/app/actions/fetchUserProfile'
+import ProfileForm from '../_components/profile-form'
 
 export const metadata: Metadata = {
-  title: "Edit profile",
-  description: "Edit profile",
-};
+  title: 'Edit profile',
+  description: 'Edit profile',
+}
 
-const EditProfile = async () => {
-  const session = await auth();
-  const profile = await fetchProfile(session?.user.username!);
-  console.log(profile);
-  if (!profile) {
-    notFound();
+export default async function EditProfile() {
+  const session = await auth()
+  if (!session) {
+    notFound()
+  }
+  const userId = session?.user.id
+
+  const profile = await fetchProfile(session?.user?.username)
+  if (!profile || session.user.id !== profile.id) {
+    notFound()
   }
 
   return (
     <div className="px-12">
       <h1 className="text-2xl font-medium">Edit profile</h1>
-
-      <ProfileForm profile={profile} />
+      <ProfileForm profile={profile} userId={userId} />
     </div>
-  );
-};
-
-export default EditProfile;
+  )
+}
